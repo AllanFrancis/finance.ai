6f9351ab4a4c8cd163f9e323746daa2ccc643843
Allan Francis <allantecnologo@gmail.com>
2024-11-06 22:04:39 -0400

    feat: add authentication with clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,16 +1,26 @@
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  const { userId } = await auth();
+  console.log(userId);
+  if (userId) {
+    redirect("/");
+  }
+
   return (
     <div className="grid h-full grid-cols-2">
+      {/* ESQUERDA */}
       <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
         <Image
           src="/logo.svg"
-          alt="Finance AI"
           width={173}
           height={39}
+          alt="Finance AI"
           className="mb-8"
         />
         <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
@@ -19,11 +29,14 @@ const LoginPage = () => {
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>
-        <Button variant={"outline"}>
-          <LogInIcon className="mr-2" />
-          Entrar com o Google
-        </Button>
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon className="mr-2" />
+            Fazer login ou criar conta
+          </Button>
+        </SignInButton>
       </div>
+      {/* DIREITA */}
       <div className="relative h-full w-full">
         <Image
           src="/login.png"
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
-import { Button } from "./_components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
   return (
     <div className="flex min-h-screen w-screen flex-col items-center justify-center gap-4">
-      <h1 className="text-primary text-3xl font-bold">finance.ai</h1>
-      <Button>Click me</Button>
+      <h1 className="text-3xl font-bold text-primary">finance.ai</h1>
+      <UserButton showName />
     </div>
   );
 }
