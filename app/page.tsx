c383e35a9c2fa2fe5397f76b6ada88a9734429d3
Allan Francis <allantecnologo@gmail.com>
2024-11-06 21:10:04 -0400

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen w-screen items-center justify-center">
-      <h1 className="text-red-500">Home</h1>
+    <div className="flex min-h-screen w-screen flex-col items-center justify-center gap-4">
+      <h1 className="text-primary text-3xl font-bold">finance.ai</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
